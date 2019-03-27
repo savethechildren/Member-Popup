e5599a06e0cdfab90c8f1b9c202a61dbb87d2a93
Damien North <damien.north@savethechildren.org>
2019-03-27 16:38:53 +0000

    Fix for popup being loaded twice.

--- dist/js/stc-popup-init.js
+++ dist/js/stc-popup-init.js
@@ -197,14 +197,16 @@ if(stc.geo.country) {
 } else {
     window.addEventListener('countryIsSet', init);
 }
+stc.popupIsInit = false;
 
 function init() {
-    if(stc.util.getCookie('stc_popup_closed') !== '1' && stc.geo.members.indexOf(stc.geo.country) > -1 && stc.geo.country !== stc.popupOrigin) {
+    if(!stc.popupIsInit && stc.util.getCookie('stc_popup_closed') !== '1' && stc.geo.members.indexOf(stc.geo.country) > -1 && stc.geo.country !== stc.popupOrigin) {
         stc.modal = stc.modal || {};
         stc.modal.baseURL = 'https://misc/member-popup/dist';
         var stcp = document.createElement('script');
         stcp.src = stc.modal.baseURL + '/js/stc-popup.min.js';
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(stcp, s);
+        stc.popupIsInit = true;
     }
 }
